8e0c332c04262c86e0881bac4d10fb97e6c8e657
Diogo Santos <santosdiogo97@gmail.com>
2018-12-14 20:08:59 +0000

    added category function

--- website/database/category.php
+++ website/database/category.php
@@ -6,4 +6,12 @@ function getAllCategories() {
     $stmt = $conn->prepare('SELECT * FROM category');
     $stmt->execute();
     return $stmt->fetchAll();
-  }
+}
+
+function getCategoryByName($name) {
+    global $conn;
+
+    $stmt = $conn->prepare('SELECT * FROM category WHERE name = ?');
+    $stmt->execute(array($name));
+    return $stmt->fetch();
+}
