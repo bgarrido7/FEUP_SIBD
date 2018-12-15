94286f2bd7478493735768bb9efbb34761016a49
Diogo Santos <santosdiogo97@gmail.com>
2018-12-15 05:24:41 +0000

    update

--- website/views/list_projects.php
+++ website/views/list_projects.php
@@ -3,17 +3,32 @@
     Category: <?= $cat_name ?>
 </h2>
 
-<div class="row">
-    <?php foreach ($products as $product) { ?>
-        <div class="column">
-            <h1><?= $product['name'] ?></h1>
-            <a class="image" href="view_proj.php?proj_id=<?php $product['projectid'] ?>">
-                <img class="image" src="<?= $product['image_path'] ?>" alt="pokemon_image" >
-            </a>
-            <p><?= $product['description'] ?></p>
+<?php if (isset($products[0])) { ?>
 
+    <div class="column right">
+        <?php foreach ($products as $product) { ?>
+            <div class="row">
+                <h1><?= $product['name'] ?></h1>
+                <a class="image" href="view_proj.php?proj_id=<?php $product['projectid'] ?>">
+                    <img class="image" src="<?= $product['image_path'] ?>" alt="pokemon_image" >
+                </a>
+                <p><?= $product['description'] ?></p>
+
+            </div>
+        <?php } ?>
+    </div>
+
+    <div class="column left">
+        <div class="">
+            filter
         </div>
-    <?php } ?>
-</div>
 
+
+    </div>
+<?php } else { ?>
+    <div class="column right">
+           No projects within this category;
+    </div>
+
+<?php } ?>
 
