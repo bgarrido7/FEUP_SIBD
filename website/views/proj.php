2ef8fdf80b6a0de33b5699f827711133074c08bd
Diogo Santos <santosdiogo97@gmail.com>
2018-12-15 22:12:48 +0000

    update

--- website/views/proj.php
+++ website/views/proj.php
@@ -1,10 +1,25 @@
-<?php
-    var_dump($display_project);
-?>
+<?php ?>
 
 <h2>
     <?= $display_project['name'] ?>
 </h2>
+
+
+<div class="left-column">
+    <img class="image" src="<?= $display_project['image_path'] ?>" alt="<?= $display_project['name'] ?>">
+
+</div>
+
+<div class="right-column">
+
+    <!-- Product Description -->
+    <div class="product-description">
+        <span>Category:<?=$display_project['category']?></span>
+        <h1><?= $display_project['name']?></h1>
+        <p><?= $display_project['description']?></p>
+    </div>
+
+    <div class=""
 
 
 
